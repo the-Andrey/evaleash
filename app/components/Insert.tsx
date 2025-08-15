429fa154383fc1038cc96fd01dc212e2b1b7d19e
the-Andrey <andreyrica.dev@gmail.com>
2025-08-14 22:21:01 -0300

    Navbar quase finalizada

---
+++ app/components/Insert.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Insert = () => {
+    // inserção os modelos
+  return (
+    <>
+        <div className='w-full h-screen bg-lawrencium3'>
+            <p>
+              ESTE É UM TESTE
+            </p>
+        </div>
+    </>
+  )
+}
+
+export default Insert
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import Navbar from "./components/Navbar";
+import Insert from "./components/Insert";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
     <>
       <Navbar></Navbar>
+      <Insert></Insert>
+      <Footer></Footer>
     </>
   );
 }
